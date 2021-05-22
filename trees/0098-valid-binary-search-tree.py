f1d912fbd775251ef23a112984d77de97eca67f3
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-05-22 16:33:19 -0300

    Solve #98 and #108

---
+++ trees/0098-valid-binary-search-tree.py
@@ -0,0 +1,27 @@
+from structures import TreeNode
+
+
+last_visited = None
+
+
+def isValidBST(root: TreeNode) -> bool:
+    return checkValid(root)
+        
+        
+def checkValid(root: TreeNode) -> bool:
+    global last_visited
+    if root is None:
+        return True
+    
+    if not checkValid(root.left):
+        return False
+    
+    if last_visited is not None and root.val <= last_visited:
+        return False
+    
+    last_visited = root.val
+    
+    if not checkValid(root.right):
+        return False
+    
+    return True
