00360adb8b2911068ee46437e65ec9b6fe5b2a10
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-05-22 15:22:21 -0300

    Add TreeNode

--- structures.py
+++ structures.py
@@ -2,3 +2,10 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
