494d39d3c6ff33b81d5d3c8b0e9fcb3686cb62e1
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-05-31 01:07:03 -0300

    Fix identation error on #108

--- trees/0108-convert-sorted-array-to-bynary-search-tree.py
+++ trees/0108-convert-sorted-array-to-bynary-search-tree.py
@@ -4,12 +4,12 @@ from structures import TreeNode
 
 
 def sortedArrayToBST(nums: List[int]) -> TreeNode:
-        if len(nums) == 0:
-            return None
-            
-        l, r = 0, len(nums) - 1
-        mid = l + (r-l) // 2
-        root = TreeNode(nums[mid])
-        root.left = sortedArrayToBST(nums[:mid])
-        root.right = sortedArrayToBST(nums[mid+1:])
-        return root
+    if len(nums) == 0:
+        return None
+        
+    l, r = 0, len(nums) - 1
+    mid = l + (r-l) // 2
+    root = TreeNode(nums[mid])
+    root.left = sortedArrayToBST(nums[:mid])
+    root.right = sortedArrayToBST(nums[mid+1:])
+    return root
