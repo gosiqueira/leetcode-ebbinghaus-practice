36456d4efe6ef22859e20bf564835f8295be9ba7
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-06-10 13:23:54 -0300

    Solve #15

---
+++ cpp/arrays/0015-3sum.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> response;
+    sort(nums.begin(), nums.end());
+        
+    for(int i=0; i < nums.size(); i++) {
+        int n = nums[i];
+        if (i > 0 && n == nums[i-1]) {
+            continue;
+        }
+        
+        int l = i + 1;
+        int r = nums.size() - 1;
+        while (l<r) {
+            int sum = n + nums[l] + nums[r];
+            if (sum > 0) {
+                r--;
+            }
+            else if (sum < 0) {
+                l++;
+            }
+            else {
+                vector<int> match({n, nums[l++], nums[r]});
+                response.push_back(match);
+                while (nums[l] == nums[l-1] and l < r) {
+                    l++;
+                }
+            }
+        }
+    }
+    
+    return response;
+}
