7e0bf793fa4753248a6584641041fa27859eddeb
gosiqueira <gosiqueira@dcc.ufmg.br>
2021-06-15 15:48:32 -0300

    Review #38 in cpp

---
+++ cpp/strings/0038-count-and-say.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string countAndSay(int n) {
+    string response = "";
+    if (n == 1) return "1";
+    if (n == 2) return "11";
+    
+    string say = countAndSay(n - 1);
+    int i = 0;
+    
+    while (i < say.length()) {
+        int count  = 0;
+        char cur = say.at(i);
+        while(i < say.length() && say.at(i) == cur) {
+            count++;
+            i++;
+        }
+        
+        response.push_back(count + '0');
+        response.push_back(cur);
+    }
+    
+    return response;
+}
