dfd3735f6eed02330884c59c029f558203e2dc0d
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-07-05 14:55:14 -0300

    Add semicolons

--- js/arrays/0048-rotate-image.js
+++ js/arrays/0048-rotate-image.js
@@ -1,16 +1,16 @@
 var rotate = function(matrix) {
     for (var layer = 0; layer < matrix.length / 2; layer++) {
-        var first = layer
-        var last = matrix.length - layer - 1
+        var first = layer;
+        var last = matrix.length - layer - 1;
         for (var i = first; i < last; i++) {
-            var offset = i - first
+            var offset = i - first;
             
-            var top = matrix[first][i]
+            var top = matrix[first][i];
             
-            matrix[first][i] = matrix[last - offset][first]
-            matrix[last - offset][first] = matrix[last][last - offset]
-            matrix[last][last - offset] = matrix[i][last]
-            matrix[i][last] = top
+            matrix[first][i] = matrix[last - offset][first];
+            matrix[last - offset][first] = matrix[last][last - offset];
+            matrix[last][last - offset] = matrix[i][last];
+            matrix[i][last] = top;
         }
     }
 };
