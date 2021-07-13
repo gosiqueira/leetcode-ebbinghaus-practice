3641d451f863c1983e5000692e88648ae94ec110
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-07-13 09:36:38 -0300

    Add custom tree node

--- cpp/structures.h
+++ cpp/structures.h
@@ -14,3 +14,18 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
