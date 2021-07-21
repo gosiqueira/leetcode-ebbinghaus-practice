2643ca80c4e997d3625b2b5f52ed24e7d03a9682
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-07-21 15:03:14 -0300

    Review #1514 in cpp

---
+++ cpp/graphs/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,36 @@
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+    vector<vector<pair<int, double>>> adj_list (n);
+    
+    for (int i = 0; i < edges.size(); i++) {
+        adj_list[edges[i][0]].push_back({edges[i][1], succProb[i]});
+        adj_list[edges[i][1]].push_back({edges[i][0], succProb[i]});
+    }
+    
+    priority_queue<pair<double, int>> q;
+    set<int> visited;
+    
+    q.push({1, start});
+    
+    while (!q.empty()) {
+        auto cur = q.top();
+        q.pop();
+        
+        if (cur.second == end) return cur.first;
+        visited.insert(cur.second);
+        
+        for (const auto neighboor : adj_list[cur.second]) {
+            if (visited.find(neighboor.first) == visited.end()) {
+                q.push({cur.first * neighboor.second, neighboor.first});
+            }
+        }
+    }
+    
+    return 0;
+}
