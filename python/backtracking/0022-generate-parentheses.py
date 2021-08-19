bb514dfb0546f9cd613e41f7b1a4bac405e84293
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-08-18 22:34:43 -0300

    Fix code style in #22

--- python/backtracking/0022-generate-parentheses.py
+++ python/backtracking/0022-generate-parentheses.py
@@ -1,13 +1,13 @@
 from typing import List
 
-
-def generateParenthesis(self, n: int) -> List[str]:
-    def generate(p, left, right):
+def generate(p, left, right):
         if right >= left >= 0:
             if not right:
                 yield p
                 
             yield from generate(p + '(', left-1, right)
             yield from generate(p + ')', left, right-1)
-            
+
+
+def generateParenthesis(self, n: int) -> List[str]:
     return list(generate('', n, n))
