a2b1381586519ed8a2e576cb3b3ab49ea711b20c
Diogenes <diogenes.pedro06@gmail.com>
2021-10-18 14:59:41 -0300

    Adding Move Zeroes solution in cpp

---
+++ cpp/arrays/0283-move-zeros.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void moveZeroes(vector<int>& nums){
+    for (int lastNonZero = 0, cur = 0; cur < nums.size; ++cur){
+        if (nums[cur] != 0)
+            swap(nums[lastNonZero++], nums[cur]);
+    }
+}
