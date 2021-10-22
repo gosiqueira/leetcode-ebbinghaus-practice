a6453bc34f78317b2aeb0740dfc6c7a75f48b0a9
Karoliny Oliveira <45614118+karolinyoliveira@users.noreply.github.com>
2021-10-22 12:34:35 -0300

    Update 0242-valid-anagram.cpp

--- cpp/strings/0242-valid-anagram.cpp
+++ cpp/strings/0242-valid-anagram.cpp
@@ -2,25 +2,29 @@
 #include <unordered_map>
 using namespace std;
 
-bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) 
-            return false;
-            
-        unordered_map<char,int> mp;
-        for(int i=0;i<s.size();i++){
-                mp[s[i]]++;
-        }
-        
-        for(int i=0;i<s.size();i++){
-            if(mp.find(t[i])==mp.end())
-                return false;
-            mp[t[i]]--;
-        }
-        
-        for(int i=0;i<s.size();i++){
-            if(mp[s[i]]!=0)
-                return false;
-        }
-        
-        return true;    
+bool isAnagram(string s, string t)
+{
+    if (s.length() != t.length())
+        return false;
+
+    unordered_map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
     }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp.find(t[i]) == mp.end())
+            return false;
+        mp[t[i]]--;
+    }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp[s[i]] != 0)
+            return false;
+    }
+
+    return true;
+}
